97b35c30066174978d4fb5a46c9040fc6ee4d80e
Daniel Burnside <burnside@Daniels-MBP.attlocal.net>
2018-06-06 21:20:01 -0400

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing-module';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { JarComponent } from './jar/jar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    JarComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
